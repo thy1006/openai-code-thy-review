af84a76b8187b821f5677f491712620f0b76a1c7
“thy1006” <3586998645@qq.com>
2025-06-07 15:34:07 +0800

    2-3-v1

--- openai-code-thy-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
+++ openai-code-thy-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
@@ -6,14 +6,35 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiTest {
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         System.out.println(Integer.parseInt("aaaa"));
+        //1.代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+
+        while ((line=reader.readLine())!=null){
+            diffCode.append(line);
+        }
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+        System.out.println("评审代码：" + diffCode.toString());
     }
 
 }
