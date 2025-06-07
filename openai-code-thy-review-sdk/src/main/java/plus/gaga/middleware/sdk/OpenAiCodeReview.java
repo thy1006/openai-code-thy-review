cd1d69f3d35f9e566b419c28c1759fba8b28777a
“thy1006” <3586998645@qq.com>
2025-06-07 15:06:33 +0800

    2-2-thy githup actions

--- openai-code-thy-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-thy-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -3,7 +3,7 @@ package plus.gaga.middleware.sdk;
 public class OpenAiCodeReview {
 
     public static void main(String[] args) {
-        System.out.println("测试执行 ");
+        System.out.println("测试执行");
     }
 
 }
