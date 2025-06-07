6d9f80b4e6b015aa745244752e19f0c7fbbece36
“thy1006” <3586998645@qq.com>
2025-06-07 15:38:30 +0800

    2-3-v1

--- openai-code-thy-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
+++ openai-code-thy-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
@@ -19,22 +19,6 @@ public class ApiTest {
     @Test
     public void test() throws Exception {
         System.out.println(Integer.parseInt("aaaa"));
-        //1.代码检出
-        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
-        processBuilder.directory(new File("."));
-        Process process = processBuilder.start();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
-
-        StringBuilder diffCode = new StringBuilder();
-
-        while ((line=reader.readLine())!=null){
-            diffCode.append(line);
-        }
-        int exitCode = process.waitFor();
-        System.out.println("Exited with code:" + exitCode);
-        System.out.println("评审代码：" + diffCode.toString());
     }
 
 }
