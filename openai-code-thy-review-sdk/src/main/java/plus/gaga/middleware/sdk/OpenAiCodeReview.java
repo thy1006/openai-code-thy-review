12cf61eafd253dc3d60138c41e86fae0565ff3ca
“thy1006” <3586998645@qq.com>
2025-06-19 17:38:49 +0800

    2-7-thy-v7

--- openai-code-thy-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-thy-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -2,7 +2,6 @@ package plus.gaga.middleware.sdk;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import plus.gaga.middleware.sdk.domain.service.IOpenAiCodeReviewService;
 import plus.gaga.middleware.sdk.domain.service.impl.OpenAiCodeReviewService;
 import plus.gaga.middleware.sdk.infrastructure.git.GitCommand;
 import plus.gaga.middleware.sdk.infrastructure.openai.IOpenAI;
